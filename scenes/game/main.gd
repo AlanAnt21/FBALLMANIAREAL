692229feb3e4fe846bcd428114cccc24ddfa6013
Mickey <91348503+WickeyMu@users.noreply.github.com>
2024-09-27 18:49:56 +1200

    made particles that work

--- scenes/game/main.gd
+++ scenes/game/main.gd
@@ -1,21 +1,30 @@
 extends Node2D
 @onready var football = $Football
 @onready var label = $Label
+@onready var particle_ronaldo = $Particle_Ronaldo
+@onready var particle_messi = $Particle_Messi
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_text()
-
+	if GameManager.score_last == "messi":
+		particle_messi.emitting = true
+	elif GameManager.score_last == "ronaldo":
+		particle_ronaldo.emitting = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("SPACE"):
 		GameManager.load_game_scene()
+		GameManager.score_last = ""
 
 
 func _on_ronaldo_goal_area_entered(_area):
 	print("MEEESSSSIIII")
 	GameManager.load_game_scene()
 	GameManager.score_messi += 1
+	GameManager.score_last = "messi"
 	
 
 
@@ -23,7 +32,7 @@ func _on_messi_goal_area_entered(_area):
 	print("SUUIIIIII")
 	GameManager.load_game_scene()
 	GameManager.score_ronaldo += 1
+	GameManager.score_last = "ronaldo"
 	
 func update_text():
 	label.text = "Score: " + str(GameManager.score_messi) + " : " + str(GameManager.score_ronaldo)
-
--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -5,5 +5,7 @@ var game_scene: PackedScene = preload("res://scenes/game/game.tscn")
 var score_messi : int = 0
 var score_ronaldo : int = 0
 
+var score_last = ""
+
 func load_game_scene():
 	get_tree().change_scene_to_packed(game_scene)
